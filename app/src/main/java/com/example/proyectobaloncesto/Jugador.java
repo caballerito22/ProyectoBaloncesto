78520fdfb7b6e02895715c54048733a0440249db
alecabnun <avestruz2265@gmail.com>
2024-11-20 13:38:40 +0100

    a

--- app/src/main/java/com/example/proyectobaloncesto/Jugador.java
+++ app/src/main/java/com/example/proyectobaloncesto/Jugador.java
@@ -1,5 +1,12 @@
 package com.example.proyectobaloncesto;
 
 public class Jugador {
-
+    int id;
+    String nombre;
+    int puntos_totales;
+    int minutos_jugados;
+    String fecha_nacimiento;
+    String imagen;
 }
+
+
