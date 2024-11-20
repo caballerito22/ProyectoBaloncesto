45f16244ce243e0e0ba46c51dd4758cf98a9e72a
alecabnun <avestruz2265@gmail.com>
2024-11-20 13:45:12 +0100

    a

--- app/src/main/java/com/example/proyectobaloncesto/Jugador.java
+++ app/src/main/java/com/example/proyectobaloncesto/Jugador.java
@@ -7,6 +7,77 @@ public class Jugador {
     int minutos_jugados;
     String fecha_nacimiento;
     String imagen;
+
+    public Jugador(int id, String nombre, int puntos_totales, int minutos_jugados, String fecha_nacimiento, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puntos_totales = puntos_totales;
+        this.minutos_jugados = minutos_jugados;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.imagen = imagen;
+    }
+    public Jugador(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntos_totales() {
+        return puntos_totales;
+    }
+
+    public void setPuntos_totales(int puntos_totales) {
+        this.puntos_totales = puntos_totales;
+    }
+
+    public int getMinutos_jugados() {
+        return minutos_jugados;
+    }
+
+    public void setMinutos_jugados(int minutos_jugados) {
+        this.minutos_jugados = minutos_jugados;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", puntos_totales=" + puntos_totales +
+                ", minutos_jugados=" + minutos_jugados +
+                ", fecha_nacimiento='" + fecha_nacimiento + '\'' +
+                ", imagen='" + imagen + '\'' +
+                '}';
+    }
 }
+
 
 
