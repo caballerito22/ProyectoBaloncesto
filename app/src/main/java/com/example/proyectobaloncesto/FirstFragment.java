17a4025022ce15a862bd6d5686296872d2c15ac9
alecabnun <avestruz2265@gmail.com>
2024-11-20 13:19:16 +0100

    a

--- app/src/main/java/com/example/proyectobaloncesto/FirstFragment.java
+++ app/src/main/java/com/example/proyectobaloncesto/FirstFragment.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.proyectobaloncesto.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    ArrayList <String> listaJugadores;
+    ArrayAdapter <String> adapter;
 
     @Override
     public View onCreateView(
@@ -21,6 +25,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
+        binding.ListViewJugadores.setAdapter(adapter);
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -29,6 +34,16 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        listaJugadores = new ArrayList<>();
+        listaJugadores.add("Lebron James");
+        listaJugadores.add("Kareem Abdul-Jabbar");
+        listaJugadores.add("Karl Malone");
+
+        adapter = new ArrayAdapter<>(getContext(),
+                R.layout.jugador_list_item,
+                R.id.textJugadorView,
+                listaJugadores);
+        binding.ListViewJugadores.setAdapter(adapter);
 
     }
 
