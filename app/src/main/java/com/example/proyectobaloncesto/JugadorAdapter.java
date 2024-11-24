6775a00ac159ad3048fc4d126f95048567767fb7
caballero22A <118520457+caballero22A@users.noreply.github.com>
2024-11-24 19:33:36 +0100

    20/11 mañana

---
+++ app/src/main/java/com/example/proyectobaloncesto/JugadorAdapter.java
@@ -0,0 +1,44 @@
+package com.example.proyectobaloncesto;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JugadorAdapter extends ArrayAdapter<Jugador> {
+    public JugadorAdapter(@NonNull Context context, int resource, @NonNull List<Jugador> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            Jugador jugador = getItem(position);
+
+            if (convertView == null){
+
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+
+                convertView = inflater.inflate(R.layout.jugador_list_item, parent, false);
+            }
+
+            TextView textoJugadorNombre = convertView.findViewById(R.id.textJugadorLista);
+            ImageView imgImage = convertView.findViewById(R.id.imgJugadorLista);
+
+            textoJugadorNombre.setText(jugador.getNombre());
+        Glide.with(getContext()).load(jugador.getImagen()).into(imgImage);
+
+            return convertView;
+    }
+}
