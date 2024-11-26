6f4cf3891a7045ffc7ce9a2d13193c44002a18fb
alecabnun <avestruz2265@gmail.com>
2024-11-26 10:11:53 +0100

    base

--- app/src/main/java/com/example/proyectobaloncesto/FirstFragment.java
+++ app/src/main/java/com/example/proyectobaloncesto/FirstFragment.java
@@ -27,6 +27,7 @@ public class FirstFragment extends Fragment {
     private FragmentFirstBinding binding;
     private ArrayList<Jugador> listaJugadores;
     private ArrayAdapter<Jugador> adapter;
+    private JugadoresViewModel model;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
