b5c524b8be248cb29db15069a7a70637c2a733e9
caballero22A <118520457+caballero22A@users.noreply.github.com>
2024-11-27 17:57:59 +0100

    25/11 mañana

--- app/src/main/java/com/example/proyectobaloncesto/MainActivity.java
+++ app/src/main/java/com/example/proyectobaloncesto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.proyectobaloncesto;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -55,13 +56,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
             return true;
         }
 
--- app/src/main/java/com/example/proyectobaloncesto/SettingsActivity.java
+++ app/src/main/java/com/example/proyectobaloncesto/SettingsActivity.java
@@ -33,18 +33,6 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-   /* @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent i = new Intent(this, SettingsActivity.class);
-            startActivity(i);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 
 }
